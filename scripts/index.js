378385e5050d2f3df23261c51d7632a1c42e7030
sht24 <agosantihernandez@outlook.com>
2022-05-01 19:11:31 -0500

    First commit

--- scripts/index.js
+++ scripts/index.js
@@ -32,6 +32,100 @@ const baseDeDatos = {
 
 // Paso a paso:
 
+const iniciarSesion = document.querySelector('.login-btn');
+const errorCont = document.querySelector('#error-container');
+
+window.addEventListener('load', () => {
+  if(localStorage.getItem('usuario')!=null){
+    accesoConcedido();
+  }else{
+    iniciarSesion.addEventListener('click',tiempoIniciandoSesion);
+
+    function tiempoIniciandoSesion(){
+      iniciarSesion.removeEventListener('click', tiempoIniciandoSesion)
+      document.querySelector('#loader').classList.remove('hidden')
+      setTimeout(verificar, 3000); 
+      errorCont.classList.add('hidden') 
+    }
+  }
+
+  function verificar() { 
+    
+    let inputEmail = document.getElementById('email-input').value;
+    let inputPassword = document.getElementById('password-input').value;
+    let email = inputEmail.toLowerCase();
+
+      for (let i = 0; i < baseDeDatos.usuarios.length; i++) {
+        if(email === baseDeDatos.usuarios[i].email && inputPassword === baseDeDatos.usuarios[i].password){
+          acceso = true
+          posicion = i
+          break;
+        }else{
+          acceso = false
+        } 
+      }
+
+      if(acceso == true){
+        let datosUsuario = { name: baseDeDatos.usuarios[posicion].name, email: baseDeDatos.usuarios[posicion].email };
+        document.querySelector('#loader').classList.add('hidden')
+        localStorage.setItem('usuario', JSON.stringify(datosUsuario))
+        accesoConcedido()
+      }else{
+        accesoDenegado(email, inputPassword);
+      } 
+  }
+
+
+
+  function accesoDenegado(email,inputPassword) {
+    document.querySelector('#loader').classList.add('hidden')
+    errorCont.classList.remove('hidden')        
+    errorCont.innerHTML =`<small>Alguno de los datos ingresados son incorrectos</small>`;
+    errorCont.style.color ='#c21736'
+    errorCont.style.textAlign ='center' 
+    errorCont.style.fontSize ='13px'
+    errorCont.style.flexWrap = 'wrap';
+    let errores = []
+    if(inputPassword.length < 5){
+      errores.unshift('La contraseña debe tener al menos 5 caracteres')
+    }
+    let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if(!regex.test(email)){
+      errores.unshift('El email no es válido')
+    }
+    if(errores.length > 0){ 
+      var lista= document.createElement('ul');
+      errorCont.appendChild(lista);
+        errores.forEach(element => {
+        lista.innerHTML += `<li>${element}</li>`;
+      });
+    }
+  }
+
+  function accesoConcedido() {
+    let formu = document.querySelector('form')
+    let titulo = document.querySelector('h1')
+    formu.classList.add('hidden')
+    titulo.classList.add('hidden')
+    let ppl = document.querySelector('main')
+    let bienvenido = document.createElement('h1');
+    ppl.appendChild(bienvenido)
+    usuario = JSON.parse(localStorage.getItem('usuario'));
+    bienvenido.innerHTML = `Bienvenido al sitio 😀 ${usuario.name}`
+    bienvenido.style.color='#ec183f'
+    ppl.innerHTML += `<button type='button' class="login-btn cerrarSesion">Cerrar sesion<button/>`
+    let cerrarSesion = document.querySelector('.cerrarSesion')
+    cerrarSesion.addEventListener('click', (e)=>{
+      localStorage.clear();
+      window.location.reload();
+    })
+  }
+});
+
+
+
+
+
 // 1) Al momento de que la persona inicia sesión, si las validaciones que ya tenemos implementadas
 // han sido exitosas, deberemos almacenar la información del usuario en el LocalStorage.
 
